6b4d0f83a657f4d51831261201b5beb371fcceea
Artur Mikhaylov <sovietmonk3y@gmail.com>
2017-10-02 19:08:18 -0700

    added an example site manifest that can be used to install the more core system dependencies for the application

---
+++ examples/site.pp
@@ -0,0 +1,20 @@
+node default {
+  include mysql::server
+
+  mysql::db { 'wh_mapper':
+    user     => 'root',
+    password => '',
+    require  => Class['mysql::server']
+  }
+
+  include stdlib
+
+  class { 'python':
+    pip     => 'present',
+    require => Class['stdlib']
+  }
+
+  class { 'wh_mapper':
+    require => [Class['python'], Package['python-pip']]
+  }
+}
