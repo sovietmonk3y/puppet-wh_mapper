9c89ff9e7acedbbc276571885ce42aa34bc251f4
Artur Mikhaylov <sovietmonk3y@gmail.com>
2017-10-04 14:52:31 -0700

    added params to the wh_mapper class in the init manifest to allow for a path to python and the wh_mapper server source directory to be provided added a file resource to the class that manages the wh_mapper systemd service config, using the template previously provided and the new class params to appropriately render the template added a service resource to the class that manages the wh_mapper systemd service

--- manifests/init.pp
+++ manifests/init.pp
@@ -1,4 +1,7 @@
-class wh_mapper {
+class wh_mapper (
+  $path_to_python = '/usr/bin/python',
+  $source_dir     = '/usr/share/wh_mapper'
+) {
   package { 'libmysqlclient-dev':
     ensure => 'present'
   }
@@ -25,5 +28,18 @@ class wh_mapper {
     ensure   => '3.1.1',
     provider => 'pip',
     require  => [Class['python'], Package['python-pip']]
+  }
+
+  file { 'wh_mapper_systemd_config':
+    ensure  => 'present',
+    content => template('wh_mapper/systemd/wh_mapper.service.erb'),
+    path    => '/etc/systemd/system/wh_mapper.service'
+  }
+
+  service { 'wh_mapper.service':
+    enable   => true,
+    ensure   => 'running',
+    provider => 'systemd',
+    require  => File['wh_mapper_systemd_config']
   }
 }
